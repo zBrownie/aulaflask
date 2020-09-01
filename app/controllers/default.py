2c7a11aa421259e2c43cc0df37c126f5a10d2d76
bruno.zenerato <bruno.zenerato@mobato.com.br>
2020-09-01 20:05:04 -0300

    first commit

---
+++ app/controllers/default.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template
+
+from app.models.forms import LoginForm
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    else:
+        print(form.errors)
+    return render_template('login.html',form=form)
+
